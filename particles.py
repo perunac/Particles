c5530b9599d74691a08ae45181b3bc5645a6be91
Pawel Wisniewski <perunac@gmail.com>
2013-09-24 23:31:37 +0200

    space changes particle type

--- particles.py
+++ particles.py
@@ -9,11 +9,11 @@
 #TODO
 #get all screen boundyce
 
-
+PARTICLESTYPES = ["PARTICLE","BOUNCYPARTICLE","BOUNCYPARTICLEDYING"]
 
 class Particle():
   """simple particle class"""
-  def __init__(self,x,y,xVel,yVel,color,screenWidth,screenHeight):
+  def __init__(self,x,y,xVel,yVel,color,screenWidth,screenHeight,startXcor,startYcor):
     self.x          = x
     self.y          = y
     self.startX     = x
@@ -23,16 +23,18 @@ class Particle():
     self.scrWidth   = screenWidth
     self.scrHeight  = screenHeight
     self.color      = color
+    self.topleftX   = startXcor
+    self.topleftY   = startYcor
 
   def update(self):
     self.x += self.xVel
     self.y += self.yVel
-    if self.x > self.scrWidth or self.y > self.scrHeight or self.x < 0 or self.y < 0:
+    if self.x > self.scrWidth or self.y > self.scrHeight or self.x < self.topleftX or self.y < self.topleftY:
       self.x = self.startX
       self.y = self.startY
     return 1
 
-class BouncyParticle():
+class BouncyParticle(object):
   """this particle bounce from walls"""
   def __init__(self,x,y,xVel,yVel,color,screenWidth,screenHeight,startXcor,startYcor):
     self.x          = x
@@ -52,4 +54,24 @@ class BouncyParticle():
       self.xVel = -self.xVel
     elif self.y > self.scrHeight or self.y < self.topleftY:
       self.yVel = -self.yVel
+    return 1
+
+class BouncyParticleDying(BouncyParticle):
+  """this particle lives specyfic amount of time"""
+  """ttl is time to live, if its -1 particle is infinite"""
+  def __init__(self,x,y,xVel,yVel,color,screenWidth,screenHeight,startXcor,startYcor,ttl):
+    BouncyParticle.__init__(self,x,y,xVel,yVel,color,screenWidth,screenHeight,startXcor,startYcor)
+    self.ttl = ttl
+
+  def update(self):
+    self.x += self.xVel
+    self.y += self.yVel
+    if self.x > self.scrWidth or self.x < self.topleftX:
+      self.xVel = -self.xVel
+    elif self.y > self.scrHeight or self.y < self.topleftY:
+      self.yVel = -self.yVel
+    if self.ttl > 0:
+      self.ttl -= 1
+    elif self.ttl == 0:
+      return 0
     return 1
--- particlesMaker.py
+++ particlesMaker.py
@@ -5,9 +5,10 @@
 #                    Version 0.1                        #
 #         http://potyczkiinformatyczne.blogspot.pl      #
 #########################################################
-import pygame,sys,particles,random
+import pygame,sys,particles,random,copy
 from pygame.locals import *
 from collections import deque
+
 
 #definitions of global constans
 SCRWIDTH  = 800
@@ -19,6 +20,13 @@ MAXXVEL = 4
 MAXYVEL = 4
 PARTICLESTOPLEFTX = 0
 PARTICLESTOPLEFTY = 0
+PARTICLESSCRWIDTH = 800
+PARTICLESSCRHEIGHT = 500
+TTL = FPS * 3  #to give particles about 3 sec span
+
+#texts center positions
+PARTTYPETXTCENTER = (150, PARTICLESSCRHEIGHT + 30)
+
 
 #colors
 #name        R    G    B
@@ -32,18 +40,29 @@ YELLOW   = (255, 255,   0)
 ORANGE   = (255, 128,   0)
 PURPLE   = (255,   0, 255)
 CYAN     = (  0, 255, 255)
+BLACK    = (  0,   0,   0)
 
-ALLCOLORS = [RED, GREEN, GRAY, BLUE, YELLOW, ORANGE, PURPLE, CYAN, NAVYBLUE]
+ALLCOLORS = [RED, GREEN, GRAY, BLUE, YELLOW, ORANGE, PURPLE, CYAN, NAVYBLUE, BLACK]
 
 
 def main():
   pygame.init()
   SCREEN = pygame.display.set_mode((SCRWIDTH,SCRHEIGHT))
   CLOCK = pygame.time.Clock()
-  particles = deque([],MAXPARTICLES)
-  mouseDown = False
-  mousePos = (None,None)
+  particleslist = deque([],MAXPARTICLES)
+  currentTypeNr = 0
 
+  mouseDown = False
+  inPartSurf = False
+  mousePos = (None,None)
+  particlesSurf = pygame.Rect(PARTICLESTOPLEFTX,PARTICLESTOPLEFTY,PARTICLESSCRWIDTH,PARTICLESSCRHEIGHT)
+
+
+  #texts
+  fontObj = pygame.font.Font('freesansbold.ttf',24)
+  partTypeText = fontObj.render(particles.PARTICLESTYPES[currentTypeNr],True,BLACK)
+  partTypeTextRect = partTypeText.get_rect()
+  partTypeTextRect.center = PARTTYPETXTCENTER
   running = True
   while running:
     SCREEN.fill(WHITE)
@@ -54,21 +73,36 @@ def main():
       elif event.type == KEYDOWN:
         if event.key == K_ESCAPE:
           pause()
+        elif event.key == K_SPACE:
+          currentTypeNr = incrementPartTypeNr(currentTypeNr)
+          (partTypeText,partTypeTextRect) = updatePartTypeText(partTypeText, partTypeTextRect, currentTypeNr, fontObj)
       elif event.type == MOUSEBUTTONDOWN:
         mouseDown = True
         mousePos = event.pos
       elif event.type == MOUSEBUTTONUP:
         mouseDown = False
       elif event.type == MOUSEMOTION:
+        if particlesSurf.collidepoint(event.pos[0],event.pos[1]):
+          inPartSurf = True
+        else:
+          inPartSurf = False
         mousePos = event.pos
 
     #add new particles if mouse button is hold
-    if mouseDown:
-      generateParticles(particles,mousePos)
+    if mouseDown and inPartSurf:
+      generateParticles(particleslist,mousePos, currentTypeNr)
     #draw particles
-    for part in particles:
-      part.update()
-      pygame.draw.circle(SCREEN,part.color,(part.x,part.y),2)
+    particlesToRemove = []
+    for part in particleslist:
+      if part.update():
+        pygame.draw.circle(SCREEN,part.color,(part.x,part.y),2)
+      else:
+        particlesToRemove.append(part)
+    for part in particlesToRemove:
+      particleslist.remove(part)
+
+    #draw text
+    SCREEN.blit(partTypeText,partTypeTextRect)
 
     pygame.display.flip()
     CLOCK.tick(FPS)
@@ -82,11 +116,36 @@ def pause():
 
 
 
-def generateParticles(list,pos):
+def generateParticles(list, pos, type):
   """generates new particles and maintaining there is not to much of then"""
-  for i in range(NEWPARTICLES/2):
-    #list.append(particles.Particle(pos[0],pos[1],random.randint(-MAXXVEL,MAXXVEL),random.randint(-MAXYVEL,MAXYVEL),ALLCOLORS[random.randint(0,len(ALLCOLORS)-1)],SCRWIDTH,SCRHEIGHT))
-    list.append(particles.BouncyParticle(pos[0],pos[1],random.randint(-MAXXVEL,MAXXVEL),random.randint(-MAXYVEL,MAXYVEL),ALLCOLORS[random.randint(0,len(ALLCOLORS)-1)],SCRWIDTH,SCRHEIGHT,PARTICLESTOPLEFTX,PARTICLESTOPLEFTY))
+  for i in range(NEWPARTICLES):
+    if particles.PARTICLESTYPES[type] == "PARTICLE":
+      print "adding normal particle"
+      list.append(particles.Particle(pos[0],pos[1],random.randint(-MAXXVEL,MAXXVEL),random.randint(-MAXYVEL,MAXYVEL),ALLCOLORS[random.randint(0,len(ALLCOLORS)-1)],PARTICLESSCRWIDTH,PARTICLESSCRHEIGHT,PARTICLESTOPLEFTX,PARTICLESTOPLEFTY))
+    elif particles.PARTICLESTYPES[type] == "BOUNCYPARTICLE":
+      print "adding bouncyparticle"
+      list.append(particles.BouncyParticle(pos[0],pos[1],random.randint(-MAXXVEL,MAXXVEL),random.randint(-MAXYVEL,MAXYVEL),ALLCOLORS[random.randint(0,len(ALLCOLORS)-1)],PARTICLESSCRWIDTH,PARTICLESSCRHEIGHT,PARTICLESTOPLEFTX,PARTICLESTOPLEFTY))
+    elif particles.PARTICLESTYPES[type] == "BOUNCYPARTICLEDYING":
+      print "adding bouncyparticledying"
+      list.append(particles.BouncyParticleDying(pos[0],pos[1],random.randint(-MAXXVEL,MAXXVEL),random.randint(-MAXYVEL,MAXYVEL),ALLCOLORS[random.randint(0,len(ALLCOLORS)-1)],PARTICLESSCRWIDTH,PARTICLESSCRHEIGHT,PARTICLESTOPLEFTX,PARTICLESTOPLEFTY,TTL))
+
+
+def incrementPartTypeNr(val):
+  if val >= len(particles.PARTICLESTYPES) - 1:
+    val = 0
+  else:
+    val += 1
+  return val
+
+def updatePartTypeText(partTypeText, partTypeTextRect, currentTypeNr, fontObj):
+  """
+  updates particle type text
+  returns pair of textSurf and rect
+  """
+  partTypeText = fontObj.render(particles.PARTICLESTYPES[currentTypeNr],True,BLACK)
+  partTypeTextRect = partTypeText.get_rect()
+  partTypeTextRect.center = PARTTYPETXTCENTER
+  return (partTypeText,partTypeTextRect)
 
 
 
